32be1465b1ff4f68278ae3960b32545598631c9f
kosvist <aarbmx6s@gmail.com>
2021-02-17 16:02:48 +0200

    timeout with shift

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 /**
  * @param {Function} cb
  * @param {Number} interval ms
+ * @param {Number} shift ms
+ * @param {Object[]} args
+ * @returns {Number} timeout id
+ */
+function timeoutEachShift(cb, interval, shift, ...args) {
+    const d = Math.floor(interval - Date.now() % interval) + shift;
+    const tid = setTimeout.apply(setTimeout, [cb, d].concat(args || []));
+    return tid;
+}
+
+/**
+ * @param {Function} cb
+ * @param {Number} interval ms
  * @param {Object[]} args
  * @returns {Number} timeout id
  */
 function timeoutEach(cb, interval, ...args) {
-    let d = Math.floor(interval - Date.now() % interval);
-    let tid = setTimeout.apply(setTimeout, [cb, d].concat(args || []));
-    return tid;
+    return timeoutEachShift(cb, interval, 0, args);
 }
 
 module.exports = {
+    timeoutEachShift,
     timeoutEach,
 };
